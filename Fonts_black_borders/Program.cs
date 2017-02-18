74024da9787444f3a346cf439c2aa3e45107ff9e
psilo <psilo@unterrainer.info>
2017-02-18 19:02:48 +0100

    Add project files.

---
+++ Fonts_black_borders/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fonts_black_borders
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
